441b5819ab810f440646bc3a24bf5b768bd10096
Rodrigo Behrmann <54535102+rodrigoBehrmann@users.noreply.github.com>
2022-07-14 16:32:19 -0300

    First Commit
    Primeiro commit , setup html,css e javascript

---
+++ script.js
@@ -0,0 +1,26 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = 900;
+canvas.height = 600;
+
+//global variables
+const cellSize = 100;
+const cellGap = 3;
+const gameGrid = [];
+
+//game board
+const controlsBar = {
+    width: canvas.width,
+    height: cellSize,
+}
+//projectiles
+//defenders
+//enemies
+//resources
+//utilities
+
+function animate(){
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(0, 0, controlsBar.width, controlsBar.height);
+    requestAnimationFrame(animate);
+}
